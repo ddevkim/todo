fcd16acbacb1efe1d368cb7aea21f68fb2bc923e
jaemin <jmk@marpple.com>
2020-08-10 18:22:45 +0900

    Create login UI

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 import { getDBTableData, updateDBTable } from "./lib/DB_handling.js";
 import { eventHandler } from "./lib/eventHandler.js";
 import { initialScreenUpdate } from "./lib/html_template.js";
+import {accountWindowControl} from "./lib/animation.js";
 Object.entries(_).map(([k, v]) => (window[k] = v));
 
 $(document).ready(() => {
+  accountWindowControl(true, true, false);
   const db_table = "todo_item_list";
-  const data = {[db_table]: getDBTableData(db_table) || []};
+  const data = { [db_table]: getDBTableData(db_table) || [] };
   const $main = $("#todo_window");
   initialScreenUpdate($main, data, db_table);
   eventHandler($main, data, db_table);
-  updateDBTable(db_table, data);
+  window.onbeforeunload = () => {
+    updateDBTable(db_table, data);
+  };
 });
