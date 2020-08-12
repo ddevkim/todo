8e7c9b5744c4f3b3b128211975fb6a82588426af
jaemin <jmk@marpple.com>
2020-08-12 17:28:10 +0900

    show/hide change for main logout button

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 import { getDBTableData } from "./lib/DB_handling.js";
 import { eventHandler } from "./lib/eventHandler.js";
 import { accountWindowControl } from "./lib/animation.js";
-import { getCurrentUser } from "./lib/data_handling.js";
+import {getCurrentUser} from "./lib/data_handling.js";
 import {initialScreenUpdate} from "./lib/eventHandler.js";
 Object.entries(_).map(([k, v]) => (window[k] = v));
 
-$(document).ready(() => {
+$(document).ready(async () => {
   const current_user = getCurrentUser();
   let item_list_data = {};
   if (!current_user) {
     accountWindowControl(true, true, false);
   }
   else {
+    accountWindowControl(false, true, false);
     item_list_data = getDBTableData(`todo_items_${current_user}`)|| {};
     initialScreenUpdate(item_list_data["item_list"], current_user );
+    $("#item_input").focus();
   }
   eventHandler(item_list_data);
 });
