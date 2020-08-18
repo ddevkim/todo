bf32634c1c490811fbf530fb2bf7d6afb4ae590d
jaemin <jmk@marpple.com>
2020-08-18 18:28:47 +0900

    change DB structure

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-import { getDBTableData } from "./lib/DB_handling.js";
-import { eventHandler } from "./lib/eventHandler.js";
+import {
+  createTables,
+  readData,
+  getSessionUserID,
+} from "./lib/DB_handling.js";
+import { eventHandlers } from "./lib/eventHandlers.js";
 import { accountWindowControl } from "./lib/animation.js";
-import {getCurrentUser} from "./lib/data_handling.js";
-import {initialScreenUpdate} from "./lib/eventHandler.js";
-Object.entries(_).map(([k, v]) => (window[k] = v));
+import { readTodoItems } from "./lib/eventHandlers.js";
 
 $(document).ready(async () => {
-  const current_user = getCurrentUser();
-  let item_list_data = {};
-  if (!current_user) {
-    accountWindowControl(true, true, false);
-  }
-  else {
-    accountWindowControl(false, true, false);
-    item_list_data = getDBTableData(`todo_items_${current_user}`)|| {};
-    initialScreenUpdate(item_list_data["item_list"], current_user );
-    $("#item_input").focus();
-  }
-  eventHandler(item_list_data);
+  const DB_accounts = "todo_user_accounts";
+  const DB_items = "todo_user_items";
+  const user_id = getSessionUserID();
+  //DB 테이블 생성
+  createTables([DB_accounts, DB_items]);
+  //이벤트 등록
+  eventHandlers();
+  //계정 접속창 제어
+  accountWindowControl(!user_id, readData(DB_accounts).length);
+  //_todo 아이템 불러오기
+  readTodoItems(user_id);
 });
